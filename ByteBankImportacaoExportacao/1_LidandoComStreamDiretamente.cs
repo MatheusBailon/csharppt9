ac2a38dd5563a1d1e61eca98d34d8125adec601d
Matheus Henrique <matheus_henrique06@hotmail.com>
2019-11-07 12:20:06 -0300

    Utilizando o StreamReader - uma classe que fa toda a manipulação do buffer e da como os dados do arquivo pode ser lido, simplificando a utilização do FileStream.

---
+++ ByteBankImportacaoExportacao/1_LidandoComStreamDiretamente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandeoComFileStream()
+        {
+            int numeroDeBytesLidos = -1;
+            string enderecoDoArquivo = "contas.txt";
+
+            //Para abrir um arquivo, basta passar o endereço dele e o modlo em que ele será utilizado, neste caso FileMode.Open
+            //FileStream = Classe que trata arquivos.
+            using (FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                //var tam = fluxoDoArquivo.Length;
+
+                //Para ler um arquivo de 1KB
+                var buffer = new byte[1024];
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+                    //tam -= numeroDeBytesLidos;
+
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+
+
+            //Deve ser utilizado para toda Stream utilizada, assim que terminar de fazer o que tem q fazer
+            //No caso do arquivo assim que terminar de ler este arquivo
+            //Como estou utilizando o using, não preciso chamar o metodo Close, pois ele é chamado automaticamente no
+            //Dispose da classe Stream (que é pai da FileStream)
+            //fluxoDoArquivo.Close();
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            //Encoding.Default => pega a codificação de texto padrão do arquivo gerado, ou melhor do sistema
+            //Operacional utilizado e retorna uma instancia desse padrão, que no nosso caso é o UTF
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -8,56 +8,28 @@ using System.Threading.Tasks;
 
 namespace ByteBankImportacaoExportacao 
 { 
-    class Program 
+    partial class Program 
     { 
         static void Main(string[] args) 
         {
-            int numeroDeBytesLidos = -1;
             string enderecoDoArquivo = "contas.txt";
 
-            //Para abrir um arquivo, basta passar o endereço dele e o modlo em que ele será utilizado, neste caso FileMode.Open
-            //FileStream = Classe que trata arquivos.
-            using (FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            //Quando temos unsings aninhado, podemos remover as chaves do mais externo e teremos a ordenação confirme abaixo
+            using(FileStream fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                //var tam = fluxoDoArquivo.Length;
-
-                //Para ler um arquivo de 1KB
-                var buffer = new byte[1024];
-                while (numeroDeBytesLidos != 0)
+                while (!leitor.EndOfStream)
                 {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-
-                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
-                    //tam -= numeroDeBytesLidos;
-                    
-                    EscreverBuffer(buffer,numeroDeBytesLidos);
+                    string linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
                 }
             }
-            
 
-            //Deve ser utilizado para toda Stream utilizada, assim que terminar de fazer o que tem q fazer
-            //No caso do arquivo assim que terminar de ler este arquivo
-            //Como estou utilizando o using, não preciso chamar o metodo Close, pois ele é chamado automaticamente no
-            //Dispose da classe Stream (que é pai da FileStream)
-            //fluxoDoArquivo.Close();
+
             Console.ReadLine();
         }
 
-        static void EscreverBuffer(byte[] buffer, int bytesLidos)
-        {
-            //Encoding.Default => pega a codificação de texto padrão do arquivo gerado, ou melhor do sistema
-            //Operacional utilizado e retorna uma instancia desse padrão, que no nosso caso é o UTF
-            var utf8 = Encoding.Default;
-
-            var texto = utf8.GetString(buffer,0,bytesLidos);
-            Console.Write(texto);
-
-            //foreach (var meuByte in buffer)
-            //{
-            //    Console.Write(meuByte);
-            //    Console.Write(" ");
-            //}
-        }
+        
     }
 } 
  
