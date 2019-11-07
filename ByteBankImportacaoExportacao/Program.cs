7eb1b9501c2e4bea1c8204f70d28a68c69d3ba34
Matheus Henrique <matheus_henrique06@hotmail.com>
2019-11-07 13:34:30 -0300

    Implementado para criar a instancia da conta corrente e lendo o arquivo como se fosse um CSV (pegando os campos separado por virgula)

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -21,12 +21,34 @@ namespace ByteBankImportacaoExportacao
                 while (!leitor.EndOfStream)
                 {
                     string linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var conta = ConverterStringParaContaCorrente(linha);
+                    var msg = $"{conta.Titular.Nome}: ag: {conta.Agencia}, num: {conta.Numero}, Saldo {conta.Saldo}";
+                    Console.WriteLine(msg);
                 }
+
             }
 
 
             Console.ReadLine();
+        }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(',');
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            //Convertendo o resultado encontrado
+            var agenciaComInt = int.Parse(agencia);
+            var numeroComInt = int.Parse(numero);
+            var saldoComDouble = double.Parse(saldo);
+
+            var conta = new ContaCorrente(agenciaComInt, numeroComInt);
+            conta.Depositar(saldoComDouble);
+            conta.Titular = new Cliente() { Nome = nomeTitular };
+            return conta;
         }
 
         
