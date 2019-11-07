4dda298201dea8a6beaa8376ea62cf9c0f0f6f01
Matheus Henrique <matheus_henrique06@hotmail.com>
2019-11-07 13:52:00 -0300

    Realizando escrita de arquivo utilizando o metodo Write da classe FileStream.

---
+++ ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            string caminhoNovoArquivo = "contasExportada.csv";
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,7895,4785.40, Gustavo Santos";
+                var encoding = Encoding.UTF8;
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -10,46 +10,13 @@ namespace ByteBankImportacaoExportacao
 { 
     partial class Program 
     { 
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            string enderecoDoArquivo = "contas.txt";
-
-            //Quando temos unsings aninhado, podemos remover as chaves do mais externo e teremos a ordenação confirme abaixo
-            using(FileStream fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDeArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
-                    string linha = leitor.ReadLine();
-                    var conta = ConverterStringParaContaCorrente(linha);
-                    var msg = $"{conta.Titular.Nome}: ag: {conta.Agencia}, num: {conta.Numero}, Saldo {conta.Saldo}";
-                    Console.WriteLine(msg);
-                }
-
-            }
-
-
+            CriarArquivo();
             Console.ReadLine();
         }
 
-        static ContaCorrente ConverterStringParaContaCorrente(string linha)
-        {
-            string[] campos = linha.Split(',');
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.', ',');
-            var nomeTitular = campos[3];
-
-            //Convertendo o resultado encontrado
-            var agenciaComInt = int.Parse(agencia);
-            var numeroComInt = int.Parse(numero);
-            var saldoComDouble = double.Parse(saldo);
-
-            var conta = new ContaCorrente(agenciaComInt, numeroComInt);
-            conta.Depositar(saldoComDouble);
-            conta.Titular = new Cliente() { Nome = nomeTitular };
-            return conta;
-        }
+        
 
         
     }
